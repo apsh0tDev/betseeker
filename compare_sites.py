77d733ec2d4ea792ce86afe97cd92878b075307f
D4vel0per <omardavidmendezmontiel@gmail.com>
2024-09-24 19:37:09 -0400

    File compare_sites.py added
    get_matches_by_name():
        -New function that finds matches by the name provided,
        if you set the "sources" arg, it will only search
        for the matches from those sources.
    
    is_match_behind():
        -Checks if a match is not updated. It takes for
        reference the scores from SOFASCORES and 365SCORES,
        and if the match's score is different, returns
        True

---
+++ compare_sites.py
@@ -0,0 +1,65 @@
+from typing import List
+from db import db
+from rich import print
+from thefuzz import fuzz
+from constants import Site
+
+def get_matches_by_name (match_name:str, sources:List[str]=[]) -> dict:
+    all_matches = db.table("live_matches").select("*")
+
+    scores = {}
+
+    if sources:
+        filters = [ f"source.eq.{src}" for src in sources ]
+        all_matches = all_matches.or_(",".join(filters))
+
+    all_matches = all_matches.execute()
+
+    for match in all_matches.data:
+        match_percent = fuzz.token_sort_ratio(match_name, match["match_name"])
+        if match_percent >= 80:
+            res_scoreboard = (
+                db.table("scoreboard")
+                .select("*")
+                .eq("match_id", match["match_id"])
+                .execute()
+            )
+
+            for m in res_scoreboard.data:
+                m["match_name"] = match["match_name"]
+                source = m["source"]
+                if source not in scores:
+                    scores[source] = m
+
+    return scores
+
+def is_match_behind(this_match:dict): # required keys: 'teamA', 'teamB', 'match_name'
+
+    other_matches = get_matches_by_name(this_match["match_name"], [Site.SOFASCORE.value(), Site.SCORES365.value()])
+    this_score = [this_match["teamA"], this_match["teamB"]]
+
+    updated_matches = []
+
+    if Site.SOFASCORE.value() in other_matches:
+        updated_matches.append(other_matches[Site.SOFASCORE.value()])
+    if Site.SCORES365.value() in other_matches:
+        updated_matches.append(other_matches[Site.SCORES365.value()])
+
+    is_behind = False
+
+    for u_match in updated_matches:
+        u_score = [u_match["teamA"], u_match["teamB"]]
+
+        if this_score != u_score:
+            is_behind = True
+
+    return is_behind
+
+# TEST:
+#   is_match_behind(
+#       {
+#           'teamA': ['6', '1'],
+#           'teamB': ['3', '1'],
+#           'match_name': "Moeller E. vs Pellegrino A."
+#       }
+#   )
